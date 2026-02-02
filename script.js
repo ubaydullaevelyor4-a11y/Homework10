884104562be49f802e5c8c0772e30515e355f6a8
ubaydullaevelyor4-a11y <ubaydullaevelyor4@gmail.com>
2026-02-02 20:46:17 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper-container',{
+    loop:true
+    autoplay: {
+        delay:3000,
+        disableOnInteraction:false
+    },
+    navigation:{
+nextEl:
+
+    }
+})
